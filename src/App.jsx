6d916f771f96e242be1893a41e741d7802542313
Dodge164 <dodge164@mail.ru>
2021-07-14 20:06:55 +0400

    исправление замечания в ревью

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 // import { Button } from 'reactstrap';
-import './App.scss';
+import './App.module.scss';
 
 function App() {
   return (
