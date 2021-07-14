35e8e6cb08a9ac5c2120f795d541588fb1beb90f
Dodge164 <dodge164@mail.ru>
2021-07-14 17:15:32 +0400

    Step 2
    Регистрация, новое React приложение, инициализация локального git,  выгрузка проекта на GitHub через gitDesktop, подключение Prettier,  Eslint, ReactStrap, SaSS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import logo from './logo.svg';
+import { Button } from 'reactstrap';
 import './App.css';
 
 function App() {
@@ -9,12 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
