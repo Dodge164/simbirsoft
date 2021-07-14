98b802169f38e96662c9bd2e1b2536646970f4f3
Dodge164 <dodge164@mail.ru>
2021-07-14 19:56:03 +0400

    создание приложения

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Button } from 'reactstrap';
-import './App.css';
+// import { Button } from 'reactstrap';
+import './App.scss';
 
 function App() {
   return (
@@ -9,7 +9,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code>src/App.js </code>
+          and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
