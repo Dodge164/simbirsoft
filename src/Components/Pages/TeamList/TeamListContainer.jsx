d063bf1452a646889a17e51e754a7de5a8d144bb
Dodge164 <dodge164@mail.ru>
2021-07-27 20:15:32 +0400

    добавлен loading на страницах

--- src/Components/Pages/TeamList/TeamListContainer.jsx
+++ src/Components/Pages/TeamList/TeamListContainer.jsx
@@ -1,20 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { getTeamsList } from '../../../Api/http';
+import Loading from '../../Loading/Loading';
 import TeamListPage from './TeamListPage';
 
 export default function TeamListPageContainer() {
   const history = useHistory();
   const [teamListPage, setTeamListPage] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(async () => {
     const teamList = await getTeamsList();
     setTeamListPage(teamList.teams);
+    setLoading(false);
   }, []);
 
   const handleClickTeam = (id) => {
     history.push(`/team-calendar/${id}`);
   };
 
-  console.log('teamListPage', teamListPage);
-  return <TeamListPage onClickTeam={handleClickTeam} teams={teamListPage} />;
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <TeamListPage onClickTeam={handleClickTeam} teams={teamListPage} />
+      )}
+    </>
+  );
 }
